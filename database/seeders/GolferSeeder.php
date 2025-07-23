b449266e183f76d338511858225f51996e3b14fc
Shoeriderr <kamilsocha20@gmail.com>
2025-07-23 09:53:41 +0200

    Task 1

--- database/seeders/GolferSeeder.php
+++ database/seeders/GolferSeeder.php
@@ -3,14 +3,26 @@
 namespace Database\Seeders;
 
 use App\Models\Golfer;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
 class GolferSeeder extends Seeder
 {
     public function run(): void
     {
+        /**
+         * @var Golfer|null
+         */
+        $lastGolfer = Golfer::orderBy('id', 'desc')->first();
+
+        /**
+         * @var int
+         */
+        $lastDebitorAccountNummer = $lastGolfer->debitor_account ?? 0;
+
         Golfer::factory()
             ->count(100)
+            ->sequence(fn (Sequence $sequence) => ['debitor_account' => $lastDebitorAccountNummer + $sequence->index + 1])
             ->create();
     }
 }
